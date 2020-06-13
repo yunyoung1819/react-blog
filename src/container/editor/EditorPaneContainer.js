24a720dd40319bca2658fa9699c0e12590744942
yunyoung1819 <yunyoung1819@hanmail.net>
2020-06-13 20:33:58 +0900

    react blog commit

--- src/container/editor/EditorPaneContainer.js
+++ src/container/editor/EditorPaneContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import EditorPane from 'components/editor/EditorPane';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -6,4 +6,32 @@ import * as editorActions from 'store/modules/editor';
 
 class EditorPaneContainer extends Component {
 
+    handleChangeInput = ({name, value}) => {
+        const { EditorActions } = this.props;
+        EditorActions.changeInput({name, value});
+    }
+
+    render() {
+        const { title, tags, markdown } = this.props;
+        const { handleChangeInput } = this;
+        return (
+            <EditorPane
+                title={title}
+                markdown={markdown}
+                tags={tags}
+                onChangeInput={handleChangeInput}
+            />
+        );
+    }
 }
+
+export default connect(
+    (state) => ({
+        title: state.editor.get('title'),
+        markdown: state.editor.get('markdown'),
+        tags: state.editor.get('tags')
+    }),
+    (dispatch) => ({
+        EditorActions: bindActionCreators(editorActions, dispatch)
+    })
+)(EditorPaneContainer);
